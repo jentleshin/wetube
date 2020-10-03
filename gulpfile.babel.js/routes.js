f4c2e5ea9df74e36e946730e8ca3716aec6e9512
jentleshin <jentleshin@gmail.com>
2020-10-04 02:11:55 +0900

    indent gulp/route.js

--- gulpfile.babel.js/routes.js
+++ gulpfile.babel.js/routes.js
@@ -1,21 +1,28 @@
 const routes = {
   dest: "dest/",
   pug: {
+    // src:
     srcFiles: "src/server/views/**/*.pug",
+    // srcMainFile:
     dest: "dest/server/views/",
+    // destFiles:
+    //destMainFIle:
   },
   server: {
+    // src:
     srcFiles: "src/server/**/*.js",
-
+    // srcMainFile:
     dest: "dest/server/",
     destFiles: "dest/server/**/*.js",
-
     destMainFile: "dest/server/init.js",
   },
   assets: {
-    src: "src/assets/js/**/*.js",
+    // src:
+    // srcFiles:
     srcMainFile: "src/assets/js/main.js",
     dest: "dest/assets/",
+    // destFiles:
+    //destMainFIle:
   },
 };
 
