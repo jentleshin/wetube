ae1ff19a743d0876a2eac53b97c2e5d60cc94215
jentleshin <jentleshin@gmail.com>
2020-10-28 02:56:34 +0900

    npm run build added

--- gulpfile.babel.js/index.js
+++ gulpfile.babel.js/index.js
@@ -1,6 +1,11 @@
 import gulp from "gulp";
 import { clearServer, prepareServer, liveServer } from "./server";
 import { clearAssets, prepareAssets, liveAssets } from "./assets";
+import del from "del";
+import routes from "./routes";
+
+const clearAll = () => del(routes.dest);
+export const build = gulp.series([clearAll, prepareServer, prepareAssets]);
 
 export const devServer = gulp.series([clearServer, prepareServer, liveServer]);
 export const devAssets = gulp.series([clearAssets, prepareAssets, liveAssets]);
